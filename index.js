4b655310e4f21e46b9c1e6f9f5992d4b1e492742
vconcept <yungaeronaut@gmail.com>
2025-02-01 06:46:00 +0100

    continous shuffling and sorting

--- index.js
+++ index.js
@@ -5,28 +5,47 @@ const rl = readline.createInterface({
     output: process.stdout
 });
 
-function insertSort(arr = [], i) {
-    rl.question("Enter a number: ", (input) => {
-        let i = parseInt(input, 10);
-    arr.push(i);
-    // for (num in arr) {
-    //     if (i > num){
+function insertSort(arr = []) {
+    function askForNumber() {
+    
+    rl.question("Enter a number (or press Enter to stop): ", (input) => {
+        if (input.trim() === "") {
+            console.log("Final sorted array:", arr);
+            rl.close();
+            return;
+        }
 
-    //     }
-    // }
-    for (let x = arr.length - 2 ; x >= 0; x--){
+        let i = parseInt(input, 10);
+        if (isNaN(i)) {
+            console.log("Invalid input.Please enter a valid number.");
+            rl.close();
+            return;
+        }
+        arr.push(i);
+        // for (num in arr) {
+        //     if (i > num){
+
+        //     }
+        // }
+        for (let x = arr.length - 2 ; x >= 0; x--){
+        // if (arr[x] > arr[x + 1]) {
+        //     let temp = arr[x];
+        //     arr[x] = arr[x + 1];
+        //     arr[x] = temp;
         if (arr[x] > i) {
-           arr[x + 1] = arr[x];
-           arr[x] = i;
+            let temp = i;
+            arr[x + 1] = arr[x];
+            arr[x] = temp;
         }
         else {
-            continue;
+            break;
         }
     }
-    console.log(arr);
-    rl.close();
+    console.log("Sorted for now", arr);
+    askForNumber();
     });
-
+    }
+askForNumber();
 }
 
 insertSort([23, 34, 67, 122]);
